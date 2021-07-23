08c7fdfec87bed852dce2b09083b5607a3b64cdf
CadisRaziel <vitorcsharp@hotmail.com>
2021-07-23 15:47:55 -0300

    inserindo funcionalidade de face cam

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,8 +7,8 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:reconhecimento_facial/core/app_widget.dart';
 
-import 'package:reconhecimento_facial/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
